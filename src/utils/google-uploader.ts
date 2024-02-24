185383e9f943ec56a25a1faef1afcaf33ce648b8
MDReal32 <veysaliyev00@gmail.com>
2024-02-24 10:13:03 +0400

    chore(gdirve): moved to core

--- src/utils/google-uploader.ts
+++
@@ -1,7 +0,0 @@
-import { GoogleDrive } from "./google-drive";
-
-export class GoogleUploader extends GoogleDrive {
-  async upload(file: string, destination: string) {
-    await this.uploadFile(file, destination);
-  }
-}
