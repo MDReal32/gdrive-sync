209bd2e9f78937ca5e5897569e7412f757268c71
MDReal32 <veysaliyev00@gmail.com>
2024-02-24 19:03:29 +0400

    chore(gdirve): package app

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,27 @@
 import { defineConfig } from "vite";
 import dtsPlugin from "vite-plugin-dts";
 
+import pkgJson from "./package.json";
+
+const nodeExternals = ["assert", "path", "fs", "os", "util", "url"];
+
 export default defineConfig({
   build: {
     outDir: "build",
     emptyOutDir: true,
     lib: {
-      name: "node-template",
+      name: "gdrive",
       entry: "src/main.ts",
       formats: ["es", "cjs"],
-      fileName: format => `node-template.${format}.js`
+      fileName: format => `gdrive.${format}.js`
+    },
+    rollupOptions: {
+      external: [
+        /^node:/,
+        ...nodeExternals,
+        ...Object.keys(pkgJson.dependencies || {}),
+        ...Object.keys(pkgJson.devDependencies || {})
+      ]
     }
   },
   plugins: [dtsPlugin({ staticImport: true, insertTypesEntry: true })],
